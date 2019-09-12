08d6bb19ef86d4c16af14dfe11c88094bc37a64c
pritamrao <pritamrao746@gmail.com>
2019-09-13 02:03:57 +0530

    Application Class for offline functionalities

---
+++ app/src/main/java/com/example/letschat/LetsChat.java
@@ -0,0 +1,26 @@
+package com.example.letschat;
+
+import android.app.Application;
+
+import com.google.firebase.database.FirebaseDatabase;
+import com.squareup.picasso.OkHttpDownloader;
+import com.squareup.picasso.Picasso;
+
+public class LetsChat extends Application
+{
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        FirebaseDatabase.getInstance().setPersistenceEnabled(true);
+
+        /*Picasso*/
+
+        Picasso.Builder builder = new Picasso.Builder(this);
+        builder.downloader(new OkHttpDownloader(this,Integer.MAX_VALUE));
+        Picasso built = builder.build();
+        built.setIndicatorsEnabled(true);
+        built.setLoggingEnabled(true);
+        Picasso.setSingletonInstance(built);
+    }
+}
