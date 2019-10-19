75e20ee96092f8c1ab25655a939c4e3042513df1
pritamrao <pritamrao746@gmail.com>
2019-10-19 18:42:06 +0530

    typing bar ui complete

--- app/src/main/java/com/example/letschat/MessageAdapter.java
+++ app/src/main/java/com/example/letschat/MessageAdapter.java
@@ -2,6 +2,7 @@ package com.example.letschat;
 
 import android.content.Context;
 import android.content.Intent;
+import android.content.res.Resources;
 import android.graphics.Color;
 import android.net.Uri;
 import android.util.Log;
@@ -13,6 +14,7 @@ import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.core.content.ContextCompat;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.google.firebase.auth.FirebaseAuth;
@@ -57,51 +59,27 @@ public class MessageAdapter extends RecyclerView.Adapter<MessageAdapter.MessageV
 
         String messageTime = TimeAgo.hhMM(mMessageList.get(position).getTime());
 
-
-        String seenStatus;
-        if(mMessageList.get(position).getSeen().equals("true")) {
-            seenStatus = "**";
-        } else {
-            seenStatus="*";
-        }
-
+        String seenStatus = mMessageList.get(position).getSeen();
 
 
         if (mMessageList.get(position).getSender().equals(mCurrentUser)) {   ///current user's message
 
             holder.messageLinearLayout.setGravity(5);   ////right messsage
-
-            holder.message.setBackgroundResource(R.drawable.message_background_white);
+            holder.message.setBackgroundColor(Color.WHITE);
             holder.message.setTextColor(Color.BLACK);
-            holder.message.setText(message + "\n"+seenStatus+"   " + messageTime);
 
-
-
-
-
-            holder.message.setGravity(5);
-
-        }
-        else {
+        } else {
             holder.messageLinearLayout.setGravity(3);                ///other peoples mesage
-
-
-
-
-            holder.message.setBackgroundResource(R.drawable.message_bachground_shape);
+            int color = ContextCompat.getColor(context,R.color.colorAccent);
+            holder.message.setBackgroundColor(color);
             holder.message.setTextColor(Color.WHITE);
-            holder.message.setText(message + "\n" + messageTime);
-
-
-            holder.message.setGravity(3);
-
         }
 
         if (mMessageList.get(position).getType().equals("text")) {
 
-           // holder.message.setBackgroundColor(Color.WHITE);
-          //  holder.message.setTextColor(Color.BLACK);
-          //  holder.message.setText(message + "\n"+seenStatus+"  " + messageTime);
+
+
+            holder.message.setText(message + "\n"+seenStatus+"  " + messageTime);
             holder.messageImage.setVisibility(View.GONE);
             holder.message.setVisibility(View.VISIBLE);
 
@@ -109,34 +87,22 @@ public class MessageAdapter extends RecyclerView.Adapter<MessageAdapter.MessageV
         }
         else if(mMessageList.get(position).getType().equals("image")){
             holder.messageImage.setVisibility(View.VISIBLE);
-            holder.message.setVisibility(View.GONE);
-
-
+            holder.message.setVisibility(View.INVISIBLE);
 
             Picasso.with(context).load(message)
                     .placeholder(R.drawable.images).into(holder.messageImage);
-
-            holder.messageImage.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View view) {
-                    Intent intent = new Intent(context,ShowImage.class);
-                    intent.putExtra("imageUrl",message);
-                    context.startActivity(intent);
-                }
-            });
 
         }
 
         else if (mMessageList.get(position).getType().equals("location")) {
 
             holder.messageImage.setVisibility(View.VISIBLE);
-            holder.message.setVisibility(View.GONE);
-            holder.messageImage.setImageResource(R.drawable.images);
+            holder.message.setVisibility(View.INVISIBLE);
+            holder.messageImage.setImageResource(R.drawable.mapdefault);
 
             holder.messageImage.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View view) {
-
                     showLocation(message);
 
                 }
@@ -145,13 +111,14 @@ public class MessageAdapter extends RecyclerView.Adapter<MessageAdapter.MessageV
         }
         else {                          /////for pdf and doc file
             holder.messageImage.setVisibility(View.VISIBLE);
-            holder.message.setVisibility(View.GONE);
-            holder.messageImage.setImageResource(R.drawable.images);
+            holder.message.setVisibility(View.INVISIBLE);
+            holder.messageImage.setImageResource(R.drawable.pdf);
             holder.messageImage.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View view) {
                     Intent webIntent = new Intent(Intent.ACTION_VIEW);
                     webIntent.setData(Uri.parse(message));
+
                     context.startActivity(webIntent);
                     Log.i("onclick", message);
 
